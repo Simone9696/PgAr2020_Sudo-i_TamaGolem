c2e3157800df09a26b8b529718b4bdb6bc119d1e
franc <gambi.francesca@gmail.com>
2020-05-29 11:40:24 +0200

    al secondo turno non si è piu inetti...prova

--- TamaGolem/src/it/unibs/ing/fp/arnaldo/tamagolem/UIO.java
+++ TamaGolem/src/it/unibs/ing/fp/arnaldo/tamagolem/UIO.java
@@ -5,7 +5,7 @@ import it.unibs.fp.mylib.InputDati;
 public class UIO {
 	
 	private final static String frame = "********************************************************";
-	private final static String MESS_GOODBYE = "\n ARRIVEDERCI \n  ";
+	private final static String MESS_GOODBYE = "\n TORNA IN ARENA \n  ";
 	private final static String MESS_WELCOME = "            SEI ENTRATO NELLA ARENA  \n       terreno di scontro dei TAMAGOLEM  \n   sarai abbastanza scaltro da comprendere \n   l'equilibrio delle forze che governano l'universo?";
 
 
@@ -14,12 +14,14 @@ public class UIO {
 	 */
 	public static void golemIntro() {
 		
+		int x = 0;
 		printWelcome();
 		
 		System.out.println("Come primo sfindante clandestino ti è concesso di scegliere il livello della sfida");
-	
+	 
 		int n = -1;
 		do {
+			
 			if (InputDati.yesOrNo("Sei un allievo ancora inetto ? Rifletti bene prima di rispondere no"))
 				Utility.beginnerAdvise();
 			else {
@@ -29,6 +31,7 @@ public class UIO {
 						Equilibrium.setMatrix(n);
 				} while (n == -2);
 			} 
+		 x++;
 		} while (n == 0);
 		
 		System.out.println();
@@ -40,6 +43,34 @@ public class UIO {
 		Battle.battle();
 		
 		printGoodbye();
+		if(Utility.newPlay())
+		do {
+			do {
+				
+				
+					do {
+						n = Utility.setElementAdvance();
+						if (n > 0)
+							Equilibrium.setMatrix(n);
+					} while (n == -2);
+				
+			 
+			} while (n == 0);
+			System.out.println();
+			
+			Golem.initializeMaxRocks();
+			Player.initializeMaxGolems();
+			Battle.initializeRockStock();
+			
+			Battle.battle();
+			
+			printGoodbye();
+			
+			
+		}
+			
+	    while(!Utility.newPlay());	
+		printGoodbyefinal();
 	}
 	
 	
@@ -58,6 +89,12 @@ public class UIO {
 	public static void printGoodbye() {
 		System.out.println(frame);
 		System.out.println(MESS_GOODBYE);
+		System.out.println(frame);
+
+	}
+	public static void printGoodbyefinal() {
+		System.out.println(frame);
+		System.out.println("BUON ALLENAMENTO ARRIVEDERCI");
 		System.out.println(frame);
 
 	}
